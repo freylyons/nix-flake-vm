64a940e20e2ca29a3bf10dc0ad25afc0372c5547
frey <freylyons@hotmail.co.uk>
2026-02-05 19:19:20 +0000

    move the virtualisation option definitions outside of the 'bare metal' definition and into the VM flake where they are defined as an extension of the bare metal module tree

--- deploy/flake.nix
+++ deploy/flake.nix
@@ -7,6 +7,48 @@
 
   outputs = { self, myVMconf }: 
   {
-    packages.x86_64-linux.default = myVMconf.nixosConfigurations.default.config.system.build.vm;
+    # extend our module tree with VM specific options
+    nixosConfigurations.default = myVMconf.nixosConfigurations.default.extendModules {
+      modules = [
+        # virtualisation
+        # this overrides the QEMU options -m, -smp by default, so this is the only way to set these options weirdly
+        #
+        # // excerpt from function definition @ nixpkgs/nixos/modules/virtualisation/qemu-vm.nix
+        # exec ${qemu-common.qemuBinary qemu} \
+        #       -name ${config.system.name} \
+        #       -m ${toString config.virtualisation.memorySize} \
+        #       -smp ${toString config.virtualisation.cores} \
+        #       -device virtio-rng-pci \
+        #       ${concatStringsSep " " config.virtualisation.qemu.networkingOptions} \
+        #       ${
+        #         concatStringsSep " \\\n    " (
+        #           mapAttrsToList (
+        #             tag: share:
+        #             "-virtfs local,path=${share.source},security_model=${share.securityModel},mount_tag=${tag}"
+        #           ) config.virtualisation.sharedDirectories
+        #         )
+        #       } \
+        #       ${drivesCmdLine config.virtualisation.qemu.drives} \
+        #       ${concatStringsSep " \\\n    " config.virtualisation.qemu.options} \
+        #       $QEMU_OPTS \
+        #       "$@"
+        # // end excerpt //
+        #
+        # You will see a discepancy between the options below and the options referenced above. This is because the 
+        # `config.virtualisation` options are not loaded by default into the nixOS module tree. To get around this,
+        # we need to wrap them with `virtualisation.vmVariant` or add this module to the imports manually.
+        {
+          virtualisation.vmVariant = {
+            virtualisation = {
+              memorySize = 2000; # default: 1024    # adjust these values and open btop in the vm to see them change on rebuild
+              cores = 4; # default: 1
+            };
+          };
+        }
+      ];
+    };
+
+    # create our executable output which launches the VM from the final configuration defined above
+    packages.x86_64-linux.default = self.nixosConfigurations.default.config.system.build.vm;
   };
 }
