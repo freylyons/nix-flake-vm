4eab9f9712a1a3ceba9ec81aabc65faee5444f73
frey <freylyons@hotmail.co.uk>
2026-02-05 18:54:44 +0000

    added virtualisation options to allow customisation of VM resources and added btop as a default program to allow conventient inspection

--- declare/configuration.nix
+++ declare/configuration.nix
@@ -5,6 +5,41 @@
 
   system.stateVersion = "24.05";
 
+  # virtualisation
+  # this overrides the QEMU options -m, -smp by default, so this is the only way to set these options weirdly
+  #
+  # // excerpt from function definition @ nixpkgs/nixos/modules/virtualisation/qemu-vm.nix
+  # exec ${qemu-common.qemuBinary qemu} \
+  #       -name ${config.system.name} \
+  #       -m ${toString config.virtualisation.memorySize} \
+  #       -smp ${toString config.virtualisation.cores} \
+  #       -device virtio-rng-pci \
+  #       ${concatStringsSep " " config.virtualisation.qemu.networkingOptions} \
+  #       ${
+  #         concatStringsSep " \\\n    " (
+  #           mapAttrsToList (
+  #             tag: share:
+  #             "-virtfs local,path=${share.source},security_model=${share.securityModel},mount_tag=${tag}"
+  #           ) config.virtualisation.sharedDirectories
+  #         )
+  #       } \
+  #       ${drivesCmdLine config.virtualisation.qemu.drives} \
+  #       ${concatStringsSep " \\\n    " config.virtualisation.qemu.options} \
+  #       $QEMU_OPTS \
+  #       "$@"
+  # // end excerpt //
+  #
+  # You will see a discepancy between the options below and the options referenced above. This is because the 
+  # `config.virtualisation` options are not loaded by default into the nixOS module tree. To get around this,
+  # we need to wrap them with `virtualisation.vmVariant` or add this module to the imports manually.
+  virtualisation.vmVariant = {
+    virtualisation = {
+      memorySize = 2000; # default: 1024    # Adjust these values and open btop in the VM to see them change on rebuild
+      cores = 4; # default: 1
+    };
+  };
+
+
   users = {
     users = {
       testUser = {
@@ -28,5 +63,6 @@
     lolcat
     cowsay
     neofetch
+    btop
   ];
 }
