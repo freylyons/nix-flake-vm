f5397333ccb5fe214d6d7fe880124c2ee8017853
frey <freylyons@hotmail.co.uk>
2026-02-05 16:38:54 +0000

    cleaned up the flake inputs and set default builds instead of hostnames to avoid faff.

--- deploy/flake.nix
+++ deploy/flake.nix
@@ -2,17 +2,11 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     myVMconf.url = "./../declare";
   };
 
-  outputs = { self, nixpkgs, myVMconf }: 
-    let
-      system = "x86_64-linux";
-      hostname = "myVM";
-    in
-    {
-
-    packages.${system}.${hostname} = myVMconf.nixosConfigurations.${hostname}.config.system.build.vm;
+  outputs = { self, myVMconf }: 
+  {
+    packages.x86_64-linux.default = myVMconf.nixosConfigurations.default.config.system.build.vm;
   };
 }
