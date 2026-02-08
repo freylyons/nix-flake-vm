099b2a26d999821e567db472ffde10937f80ca8f
frey <freylyons@hotmail.co.uk>
2026-02-08 15:14:24 +0000

    added system locale settings

--- declare/configuration.nix
+++ declare/configuration.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }:
 {
+  # boot loader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   system.stateVersion = "24.05";
 
-
-
+  # users
   users = {
     users = {
       testUser = {
@@ -25,7 +25,13 @@
       scriptRunner = {};
     };
   };
+
+  # Locale
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console.keyMap= "uk";
   
+# system packages
   environment.systemPackages = with pkgs; [
     lolcat
     cowsay
